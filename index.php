719d2916af0a54763b834cdd15a519ed68e32005
Art <artsvicencio@gmail.com>
2017-04-01 18:47:10 +0800

    Added JQuery 3.2 and move JS file to separate folder (/JS)

--- index.php
+++ index.php
@@ -1,30 +1,28 @@
-<?php 
-    echo("
-        <html>
-            <head>
-                <title>File Content Manager</title>
-                <style type='text/css'>
-                    .droppable-area {
-                        background: #08c;
-                        color: #fff;
-                        padding: 100px 0;
-                        text-align: center;
-                    }
-                    .droppable-area.dragover {
-                        background: #00CC71;
-                    }
-                </style>
-            </head>
-            <body>
-                <div class='droppable-area'>
-                    <p>Drag files here or click to upload</p>
-                </div>
-                <div class='output'></div>
-                
-                <!-- Import excel-dragdrop-reader.js  -->
-                <script src='excel-dragdrop-reader.js'></script>
+<html>
+    <head>
+        <title>File Content Manager</title>
+        <style type='text/css'>
+            .droppable-area {
+                background: #08c;
+                color: #fff;
+                padding: 100px 0;
+                text-align: center;
+            }
+            .droppable-area.dragover {
+                background: #00CC71;
+            }
+        </style>
+    </head>
+    <body>
+        <div class='droppable-area'>
+            <p>Drag files here or click to upload</p>
+        </div>
+        <div class='output'></div>
+        
+        <!-- Import jquery 3.2 -->
+        <script src='JS/jquery.3.2.min.js'></script>
+        <!-- Import excel-dragdrop-reader.js  -->
+        <script src='JS/excel-dragdrop-reader.js'></script>
 
-            </body>
-        </html>
-    ");
-?>
+    </body>
+</html>
